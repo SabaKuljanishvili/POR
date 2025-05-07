9f543de81d8bb9da6d9ffe12367786c876286fcc
saba kuljanishvili <kuljanisaba@gmail.com>
2025-05-07 21:59:50 +0400

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from "./footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: "main", pathMatch: "full"  }, 
+    {path: 'main', component: MainComponent},
+    // {path: 'cancel', component: CancelComponent},
+    // {path: 'ticket', component: TicketComponent},
+    // {path: 'about', component: AboutComponent},
+    {path: '**', component: ErrorComponent },
+
+
+
+    {path: "", redirectTo: "main", pathMatch: "full"},
+   
+    {
+       path: "main",
+       title: "Main",
+       loadComponent: () => import('./main/main.component').then(m => m.MainComponent)
+    },
+//     {
+//       path: "cancel",
+//       title: "Tickets cancellation",
+//       loadComponent: () => import('./cancel/cancel.component').then(m => m.CancelComponent)
+//    },
+//    {
+//       path: "ticket",
+//       title: "Tickets",
+//       loadComponent: () => import('./ticket/ticket.component').then(m => m.TicketComponent)
+//    },
+//     {
+//         path: "about",
+//         title: "About",
+//         loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)
+//      },
+];
